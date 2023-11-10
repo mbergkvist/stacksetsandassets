d2464d6c179e7342e95a650e08aeee7c644d7ad2
Markus Bergkvist <mbergkvist@users.noreply.github.com>
2023-11-10 09:48:19 +0100

    Update application with stacksets and lambda asset

---
+++ bin/stacksetsandassets.MyNodejsFunction.ts
@@ -0,0 +1,1 @@
+export const handler = async () => {console.log('Hello, world!');};
--- bin/stacksetsandassets.ts
+++ bin/stacksetsandassets.ts
@@ -1,21 +1,66 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { StacksetsandassetsStack } from '../lib/stacksetsandassets-stack';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import {
+  Capability,
+  StackSet,
+  StackSetStack,
+  StackSetStackProps,
+  StackSetTarget,
+  StackSetTemplate,
+} from 'cdk-stacksets';
+import { Construct } from 'constructs';
+import path = require('path');
+
+export class MyStackSetStack extends StackSetStack {
+  constructor(scope: Construct, id: string, props: StackSetStackProps = {}) {
+    super(scope, id, props);
+
+    new NodejsFunction(this, 'MyNodejsFunction');
+  }
+}
+
+export class MyStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: cdk.StackProps = {}) {
+    super(scope, id, props);
+
+    const bucket = new s3.Bucket(this, 'Assets', {
+      bucketName: 'mbergkvist-cdkstacket-asset-bucket-123',
+    });
+
+    bucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        actions: ['s3:Get*', 's3:List*'],
+        resources: [bucket.arnForObjects('*'), bucket.bucketArn],
+        principals: [new iam.OrganizationPrincipal('o-123abc')],
+      }),
+    );
+
+    const stackSetStack = new MyStackSetStack(this, 'MyStackSetStack', {
+      assetBucket: bucket,
+    });
+
+    new StackSet(this, 'StackSet', {
+      target: StackSetTarget.fromAccounts({
+        regions: ['eu-central-1'],
+        accounts: ['123456789012'],
+      }),
+      capabilities: [Capability.NAMED_IAM],
+      template: StackSetTemplate.fromStackSetStack(stackSetStack),
+    });
+  }
+}
+
+// for development, use account/region from cdk cli
+const devEnv = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
 
 const app = new cdk.App();
-new StacksetsandassetsStack(app, 'StacksetsandassetsStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+new MyStack(app, 'cdk-stacksets-demo-dev', { env: devEnv });
+// new MyStack(app, 'cdk-stacksets-demo-prod', { env: prodEnv });
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+app.synth();
